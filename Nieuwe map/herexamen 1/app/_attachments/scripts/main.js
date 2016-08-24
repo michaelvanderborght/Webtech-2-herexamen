3589a54bfb37fc5aa47d964a92a0ebbbe89b34ff
michaelvanderborght <michael.vanderborght.mv@gmail.com>
2016-08-24 14:25:38 +0200

    added createDoc function

---
+++ Nieuwe map/herexamen 1/app/_attachments/scripts/main.js
@@ -0,0 +1,35 @@
+function createDoc(){
+	var gerecht = $("gerecht").val();
+	var hoeveelheid = $("#hoeveelheid").val();
+	var tafel = $("#tafel").val();
+	var opmerkingen = $("#opmerkingen").val();
+	var tijd = $("#tijd").val();
+	
+	var doc = {};
+	
+	doc.gerecht = gerecht;
+	doc.hoeveelheid = hoeveelheid;
+	doc.tafel = tafel;
+	doc.opmerkingen = opmerkingen;
+	doc.tijd = tijd;
+	
+	var json = JSON.stringify(doc);
+	
+	$.ajax({
+        type : 'PUT',
+        url : 
+        data : json,
+        contentType : 'application/json',
+        async : true,
+        success : function(data){
+        	$("gerecht").val('');
+            $("#hoeveelheid").val('');
+            $("#tafel").val('');
+            $("#opmerkingen").val('');
+            $("#tijd").val('');
+        },
+        error : function(XMLHttpRequest, textStatus, errorThrown){
+            console.log(textStatus);
+        }
+    });
+}
